53615d86d7a28f253e9b09479f559377f76fec82
N-Sindhu <sindhunarahari1309@gmail.com>
2023-11-22 20:30:50 +0530

    add flies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { MOCK_DATA } from './mock.const';
+import { FormBuilder } from '@angular/forms';
+interface MockDatas {
+  id: Number;
+  first_name: String;
+  last_name: String;
+  email: String;
+  gender: String;
+  avatar: String;
+  domain: String;
+  available: Boolean
+
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +20,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Project_Helivers';
+  mockdatas: any;
+  searchForm: any;
+  cartData: any[]=[];
+  constructor(private fb: FormBuilder) { }
+
+  
+
+  ngOnInit(): void {
+    this.mockdatas = MOCK_DATA;
+    this.searchForm = this.fb.group({
+      domain: [''],
+      gender: [''],
+      available: [''],
+
+    });
+    
+  }
+  searchByName(data: any) {
+    if (data.target.value) {
+      this.mockdatas = MOCK_DATA.filter(item => item.first_name.toUpperCase() === data.target.value.toUpperCase());
+
+    } else {
+      this.mockdatas = MOCK_DATA;
+    }
+  }
+  searchData(data: any, value: string) {
+    if (value === 'search') {
+      this.mockdatas = MOCK_DATA.filter(item => item.domain.toUpperCase() === data.domain.toUpperCase() && item.gender.toUpperCase() === data.gender.toUpperCase() && item.available.toString() === data.available);
+
+    } else {
+      this.mockdatas=MOCK_DATA.values;
+      
+    }
+
+
+  }
+  addcart(data:any){
+    const value:any = this.cartData.find(item => item.domain === data.domain);
+     console.log(value)
+    if(value && value.domain !== data.domain){
+      this.cartData.push(
+        { "id": data.id, "first_name": data.first_name, "last_name": data.last_name, "email": data.email, "gender": data.gender, "avatar": data.avatar, "domain": data.domain, "available": data.available 
+      },)
+    }else if(value === undefined){
+      this.cartData.push(
+        { "id": data.id, "first_name": data.first_name, "last_name": data.last_name, "email": data.email, "gender": data.gender, "avatar": data.avatar, "domain": data.domain, "available": data.available 
+      },)
+    }
+  }
 }
